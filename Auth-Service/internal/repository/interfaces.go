648937613f2ec9f042af7de96bcdd75cff1cb0f1
Fabio-Alfredo <00210121@uca.edu.sv>
2026-02-13 22:11:27 -0600

    :sparkles: feat: refactor interface for user repository

--- Auth-Service/internal/repository/interfaces.go
+++ Auth-Service/internal/repository/interfaces.go
@@ -1,10 +1,11 @@
 package repository
 
 import (
+	"Auth-Service/internal/domain"
 	"context"
 )
 
 type IUserRepository interface {
-	Save(ctx context.Context, data *User) error
+	Save(ctx context.Context, data *domain.User) error
 	FindEmail(ctx context.Context, email string) (*User, error)
 }
