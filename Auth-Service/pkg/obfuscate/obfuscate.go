b298511580c4b6eb59cc814bf464ac8360fd395a
Fabio-Alfredo <00210121@uca.edu.sv>
2026-08-04 21:29:29 -0600

    :sparkles: feat: update obfuscate methods

--- Auth-Service/pkg/obfuscate/obfuscate.go
+++ Auth-Service/pkg/obfuscate/obfuscate.go
@@ -1,8 +1,8 @@
 package obfuscate
 
 import (
-	"Auth-Service/internal/domain"
 	"Auth-Service/internal/domain/dtos"
+	"Auth-Service/internal/domain/models"
 	"strings"
 )
 
@@ -12,13 +12,23 @@ const (
 )
 
 // Register Method for obfuscate password in register user
-func RegisterService(user domain.User) domain.User {
+func RegisterService(user models.User) models.User {
 	user.Password = charObfuscatePass
 	return user
 }
 
 func RegisterController(req dtos.RegisterRequest) dtos.RegisterRequest {
 	req.User.Password = charObfuscatePass
+	return req
+}
+
+func Password(req models.User) models.User {
+	req.Password = charObfuscatePass
+	return req
+}
+
+func TokenAuth(req models.Token) models.Token {
+	req.Token = charObfuscate
 	return req
 }
 
